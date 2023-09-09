e8a778bc4e377e711b89cc3bd71acb060f4a7eb0
katomoti <515360446@qq.com>
2023-09-09 15:08:32 +0800

    IO流，统计文件大小

--- test/src/Main.java
+++ test/src/Main.java
@@ -1,5 +1,29 @@
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        File file = new File("D:\\Downloads\\RiceShower");
+
+        System.out.println(HowBig(file));
+    }
+
+    public static long HowBig(File file){
+        long sum = 0;//文件夹大小
+        File[] files = file.listFiles();//获取当前文件夹下的所有内容
+        if (files != null){
+            for (File f1: files){//遍历当前文件夹下的内容
+                //判断是否为文件
+                if (f1.isFile()){
+                    //是文件
+                    sum += f1.length();
+                }else {
+                    //是文件夹
+                    sum += HowBig(f1);
+
+                }
+            }
+        }
+        return sum;
     }
 }
